42776d20584ea7d90f36ca5b3371fcd8b9443ba8
Alex Titan <aburnap@aburnap.com>
2017-02-03 15:19:51 -0500

    allow probability estimates to differential privacy sgd objective

--- sklearn/linear_model/stochastic_gradient.py
+++ sklearn/linear_model/stochastic_gradient.py
@@ -728,7 +728,7 @@ class SGDClassifier(BaseSGDClassifier):
     def _check_proba(self):
         check_is_fitted(self, "t_")
 
-        if self.loss not in ("log", "modified_huber"):
+        if self.loss not in ("log", "modified_huber", "logdp"):
             raise AttributeError("probability estimates are not available for"
                                  " loss=%r" % self.loss)
 
